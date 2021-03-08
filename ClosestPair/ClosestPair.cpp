e0910652b776e9e97dbe0f6ae66ab7f26635d379
Hari Prasad S A <55315604+HariPrasad5724@users.noreply.github.com>
2021-03-08 18:58:42 +0530

    Closest pair commit 3 Tried Plotting

--- ClosestPair/ClosestPair.cpp
+++ ClosestPair/ClosestPair.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
+#include<windows.h>
+#include<stdio.h>
+#include<cstdlib>
 using namespace std;
+
+#define LT 218
+#define RT 191
+#define CROSS 197
+#define HL 196
+#define VL 179
+#define TT 194
+#define BT 193
+#define LST 195
+#define RST 180
+#define LB 192
+#define RB 217
+
+#define BS 8
+#define SPACE 32
 
 class Graph
 {
@@ -97,6 +115,75 @@ float middlePoints(Graph points[], int size, int smallest_distance)
     return middleClosest(middlePoints, j, smallest_distance);
 }
 
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void drawGraph()
+{
+	int X=10,Y=5;
+	float val=0;
+	for(int i=0;i<21;i++,Y++){
+		gotoxy(X,Y);
+		cout << char(VL);
+	}
+	gotoxy(X,Y);
+	cout << char(LB);
+	X+=1;
+
+	for(int i=0;i<52;i++,X++)
+	{
+		gotoxy(X,Y);
+		cout << char(HL);
+	}
+	X=12;
+	for(int i=0;i<6;i++,X+=10)
+	{
+		gotoxy(X,Y);
+		cout << char(TT);
+	}
+	Y+=1;
+	X=11;
+	for(int i=0;i<6;i++,X+=10)
+	{
+		gotoxy(X,Y);
+		printf("%0.1f",val/10);
+		val+=2;
+	}
+	X=10;
+	Y=5;
+	for(int i=0;i<6;i++,Y+=4)
+	{
+		gotoxy(X,Y);
+		cout << char(RST);
+	}
+	X=6;
+	Y=5;
+	val=10;
+	for(int i=0;i<6;i++,Y+=4)
+	{
+		gotoxy(X,Y);
+		printf("%0.1f",val/10);
+		val-=2;
+	}
+
+	gotoxy(X,Y+5);
+}
+
+void plotPoints(Graph P[],int n)
+{
+	int X=15,Y=24;
+	for(int i=0;i<n;i++)
+	{
+		gotoxy(X+(P[i].x_coord)*50,Y-(P[i].y_coord)*20);
+		cout << ".";
+	}
+}
+
 int main()
 {
     srand((unsigned int)time(0));
@@ -107,6 +194,11 @@ int main()
         randomPoints[i].x_coord = rand() % 100;
         randomPoints[i].y_coord = rand() % 100;
     }
+	//drawGraph();
+
+	int n = sizeof(randomPoints) / sizeof(randomPoints[0]);
+	//plotPoints(randomPoints,n);
+	//gotoxy(0,30);
 
     sort(randomPoints, randomPoints + 100, compareTwoPointX);
     printElements(randomPoints);
